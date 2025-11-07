226c1361f4aef2192c67c0774d145b142a0a2527
ibrahim0891 <nijhum0891@gmail.com>
2025-11-07 17:30:02 +0600

    feat(home): implement Feaurte,pricing , cta section.
    refactor: create a separate `constant` folder to store all object, arrays and value used by iteration accross different pages

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,15 @@
+import CTASection from "@/components/(home)/home/CallToAction";
+import Features from "@/components/(home)/home/Features";
 import Hero from "@/components/(home)/home/Hero";
+import Pricing from "@/components/(home)/home/PricingSection";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
+      <Features/>
+      <Pricing/>
+      <CTASection/>
     </div>
   );
 };
